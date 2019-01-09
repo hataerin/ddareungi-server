ab3c685675ff9b96dd7bffdc8b5ad7de3668aa7e
hataerin <goil1113@likelion.org>
2019-01-09 07:39:08 +0000

    crawl1.rb/login, test crawling

---
+++ crawl1.rb
@@ -0,0 +1,39 @@
+require 'selenium-webdriver'
+
+Selenium::WebDriver::Chrome.driver_path = `which chromedriver-helper`.chomp # 설치한 크롬 드라이버 사용
+options = Selenium::WebDriver::Chrome::Options.new # 크롬 헤드리스 모드 위해 옵션 설정
+options.add_argument('--disable-gpu') # 크롬 헤드리스 모드 사용 위해 disable-gpu setting
+options.add_argument('--headless') # 크롬 헤드리스 모드 사용 위해 headless setting
+@browser = Selenium::WebDriver.for :chrome, options: options # 실레니움 + 크롬 + 헤드리스 옵션으로 브라우저 실행
+
+browser = @browser.get 'https://www.bikeseoul.com/login.do' # 로그인 페이지 가기
+
+id_input = "//*[@id=\"memid\"]" # email 입력 부분
+pw_input = "//*[@id=\"mempw\"]" # pw 입력 부분
+
+inputID = @browser.find_element(xpath: id_input)
+inputID.send_keys "taerin97" # id 입력
+inputPW = @browser.find_element(xpath: pw_input)
+inputPW.send_keys "fnqlghkdlxld!1" # pw 입력
+
+@browser.find_element(xpath: "/html/body/div[1]/div/div/div/form/div[4]/a").click # 제출 버튼
+
+
+# (1..1145).each do |i|                 
+#   puts i
+#   begin
+#     @browser.navigate.to "http://uni.likelion.org/users/#{i}"
+#     puts "학생 : " + @browser.find_element(xpath: "/html/body/main/header/div/div/h1").text
+#     puts "email : " + @browser.find_element(xpath: "/html/body/main/header/div/p[1]/span").text.strip.split("\n").first
+#     puts "기수 : " + @browser.find_element(xpath: "/html/body/main/header/div/p[2]/span").text.strip.split(" ")[0].split("년부터").first
+#     puts "학교 : " + @browser.find_element(xpath: "/html/body/main/header/div/p[2]/span").text.strip.split(" ")[4]
+#     puts "------------------------"
+#   rescue
+#     puts "없습니다."
+#   end
+# end
+
+@browser.navigate.to "https://www.bikeseoul.com/app/mybike/memberInfo/memberInfoEdit.do"
+@x = puts @browser.find_element(xpath: "/html/body/div[1]/div[2]/div[2]/div[2]/form/div[1]/table/tbody/tr[1]/td/span").text + "님 환영합니다!"
+
+@browser.quit
